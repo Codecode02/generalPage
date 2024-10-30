be1924b059654e9c7f09a69749e2e45834db87d3
Samvel Antonyan <samvelantonyan1998@gmail.com>
2024-10-30 22:09:31 +0400

    add IslandPulseGuide projects home and Revies

---
+++ reviews.js
@@ -0,0 +1,27 @@
+const reviewBlocks = document.querySelectorAll('.reviews');
+
+reviewBlocks.forEach(block => {
+    const slides = block.querySelectorAll('.slide');
+    let currentIndex = 0;
+
+    
+    function showSlide(index) {
+        slides.forEach(slide => slide.classList.remove('active'));
+        slides[index].classList.add('active');
+    }
+
+    block.querySelector('.arrowLeft').addEventListener('click', () => {
+        currentIndex = (currentIndex > 0) ? currentIndex - 1 : slides.length - 1;
+        showSlide(currentIndex);
+    });
+
+    block.querySelector('.arrowRight').addEventListener('click', () => {
+        currentIndex = (currentIndex < slides.length - 1) ? currentIndex + 1 : 0;
+        showSlide(currentIndex);
+    });
+
+
+    showSlide(currentIndex);
+});
+
+document.getElementById('block1').classList.add('active');
